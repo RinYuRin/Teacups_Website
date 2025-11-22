63e3dfa6fbbf20e75e7475ab5a1ef27b697aa08c
RinYuRin <orakawayukirin102903@gmail.com>
2025-11-23 03:50:19 +0800

    fix menu problem

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+// These lines define __dirname manually for ES Modules
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        menu: resolve(__dirname, 'menu.html'),
+      },
+    },
+  },
 })
